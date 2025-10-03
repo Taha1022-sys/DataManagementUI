1bc469146193cedab4620c9a6cc39fa8ae97e42d
Taha Furkan Teke <tahafurkan.39@gmail.com>
2025-10-03 22:03:41 +0300

    Add proxy configuration for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173, 
+    proxy: {
+      '/api': {
+        target: 'https://data-management-api-tft-e7cghghbhyffabg3.francecentral-01.azurewebsites.net',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
